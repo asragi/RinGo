3f19db4ff3b7daf49dd9f0b67ee820577a1d3424
Ragi <ragi-justaway@hotmail.co.jp>
2024-01-01 23:15:13 +0900

    add item list endpoint

---
+++ endpoint/get_item_list.go
@@ -0,0 +1,41 @@
+package endpoint
+
+import (
+	"fmt"
+
+	"github.com/asragi/RinGo/core"
+	"github.com/asragi/RinGo/stage"
+	"github.com/asragi/RingoSuPBGo/gateway"
+)
+
+type getItemEndpoint func(*gateway.GetItemListRequest) (*gateway.GetItemListResponse, error)
+
+func CreateGetItemService(
+	getItem stage.GetItemListFunc,
+) getItemEndpoint {
+	get := func(req *gateway.GetItemListRequest) (*gateway.GetItemListResponse, error) {
+		userId := core.UserId(req.UserId)
+		res, err := getItem(userId)
+		if err != nil {
+			return &gateway.GetItemListResponse{}, fmt.Errorf("error on get item list endpoint: %w", err)
+		}
+		itemList := func(res []stage.ItemListRow) []*gateway.GetItemListResponseRow {
+			result := make([]*gateway.GetItemListResponseRow, len(res))
+			for i, v := range res {
+				result[i] = &gateway.GetItemListResponseRow{
+					ItemId:      string(v.ItemId),
+					DisplayName: string(v.DisplayName),
+					Stock:       int32(v.Stock),
+					MaxStock:    int32(v.MaxStock),
+					Price:       int32(v.Price),
+				}
+			}
+			return result
+		}(res)
+		return &gateway.GetItemListResponse{
+			ItemList: itemList,
+		}, nil
+	}
+
+	return get
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/asragi/RinGo
 
 go 1.21
 
-require github.com/asragi/RingoSuPBGo v0.0.0-20240101042638-53a67fe67a9c
+require github.com/asragi/RingoSuPBGo v0.0.0-20240101135430-87d7f5b43d65
 
 require google.golang.org/protobuf v1.31.0 // indirect
