5deee3d22cf5697f16bc51bbbe313c0f17936770
Ragi <ragi-justaway@hotmail.co.jp>
2024-01-01 14:53:23 +0900

    add get item detail endpoint

---
+++ endpoint/get_item_detail.go
@@ -0,0 +1,54 @@
+package endpoint
+
+import (
+	"fmt"
+
+	"github.com/asragi/RinGo/core"
+	"github.com/asragi/RinGo/stage"
+	"github.com/asragi/RingoSuPBGo/gateway"
+)
+
+type getItemDetailEndpointRes func(*gateway.GetItemDetailRequest) (*gateway.GetItemDetailResponse, error)
+
+func CreateGetItemDetail(
+	getItemDetail stage.GetItemDetailFunc,
+) getItemDetailEndpointRes {
+	get := func(req *gateway.GetItemDetailRequest) (*gateway.GetItemDetailResponse, error) {
+		userId := core.UserId(req.UserId)
+		itemId := core.ItemId(req.ItemId)
+		token := core.AccessToken(req.Token)
+		handleError := func(err error) (*gateway.GetItemDetailResponse, error) {
+			return &gateway.GetItemDetailResponse{}, fmt.Errorf("error on get item detail endpoint: %w", err)
+		}
+		res, err := getItemDetail(stage.GetUserItemDetailReq{
+			UserId:      userId,
+			ItemId:      itemId,
+			AccessToken: token,
+		})
+		if err != nil {
+			return handleError(err)
+		}
+		explores := func(explores []stage.UserExplore) []*gateway.UserExplore {
+			result := make([]*gateway.UserExplore, len(explores))
+			for i, v := range explores {
+				result[i] = &gateway.UserExplore{
+					ExploreId:   string(v.ExploreId),
+					DisplayName: string(v.DisplayName),
+					IsKnown:     bool(v.IsKnown),
+					IsPossible:  bool(v.IsPossible),
+				}
+			}
+			return result
+		}(res.UserExplores)
+		return &gateway.GetItemDetailResponse{
+			UserId:      string(res.UserId),
+			ItemId:      string(res.ItemId),
+			Price:       int32(res.Price),
+			MaxStock:    int32(res.MaxStock),
+			Stock:       int32(res.Stock),
+			UserExplore: explores,
+		}, nil
+	}
+
+	return get
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/asragi/RinGo
 
 go 1.21
 
-require github.com/asragi/RingoSuPBGo v0.0.0-20231230164638-e888cb47417d
+require github.com/asragi/RingoSuPBGo v0.0.0-20240101042638-53a67fe67a9c
 
 require google.golang.org/protobuf v1.31.0 // indirect
